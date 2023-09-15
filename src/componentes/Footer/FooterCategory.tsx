260ea37dd4f8e5f410176bf77a119db8c00f7c42
Gabriel Padilha da Silva <gabriel@crpmango.com.br>
2023-09-15 12:19:18 -0300

    feat/ categories mobile

--- src/componentes/Footer/FooterCategory.tsx
+++ src/componentes/Footer/FooterCategory.tsx
@@ -1,19 +1,41 @@
+import { useState } from "react";
 import { CategoriesContent, CategoryTitle } from "./Footer.styled"
 import FooterSubCategory from "./FooterSubCategory"
 import { FooterCategoryItemType, FooterSubCategoryItemType } from "./typez"
 
 function FooterCategory({title,subcategories}:FooterCategoryItemType){
-
+    const isMobile = window.innerWidth <= 768;
+    const [openCategoryMobile,setOpenCategoryMobile] = useState(false)
     return(
         <CategoriesContent>
-            <CategoryTitle>{title}</CategoryTitle>
             {
-                subcategories?.map((item:FooterSubCategoryItemType,key:any)=>{
-                    return(
-                        <FooterSubCategory key={key}tititulosubcategory={item.tititulosubcategory}/>
-                    )
-                })
+                isMobile ? 
+                <>
+                <CategoryTitle onClick={()=>setOpenCategoryMobile(!openCategoryMobile)}>{title} {openCategoryMobile? "-" : "+"}</CategoryTitle>
+                            { openCategoryMobile ?
+                                subcategories?.map((item:FooterSubCategoryItemType,key:any)=>{
+                                    return(
+                                        <FooterSubCategory key={key}tititulosubcategory={item.tititulosubcategory}/>
+                                    )
+                                }):
+                                ""
+                            }
+
+                </>
+                :
+                <>
+                <CategoryTitle>{title}</CategoryTitle>
+                            {
+                                subcategories?.map((item:FooterSubCategoryItemType,key:any)=>{
+                                    return(
+                                        <FooterSubCategory key={key}tititulosubcategory={item.tititulosubcategory}/>
+                                    )
+                                })
+                            }
+
+                </>
             }
+            
         </CategoriesContent>
     )
 }
